7f247d8121c2d14c3c0313d070893f034421aa4e
Robson <robsondgn@outlook.com>
2021-08-17 02:02:25 -0300

    Navbar

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product';
+import useStyles from './styles';
 
 const products = [
   { id: 1, name: 'Shoes', description: 'Running shoes.', price: '$5', image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cushion-shoes-7659-1584132587.jpg?crop=1.00xw:0.701xh;0,0.229xh&resize=1200:*' },
@@ -9,8 +10,11 @@ const products = [
 ]
 
 const Products = () => {
+  const classes = useStyles();
+
   return (
-    <main>
+    <main className={classes.content}>
+      <div className={classes.toolbar} />
       <Grid container justify="center" spacing={4}>
         {products.map((product) => (
           <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
